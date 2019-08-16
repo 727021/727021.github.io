eded5de3bf8e22db091acc6a9cff521a763f5d87
727021 <1834544+727021@users.noreply.github.com>
2019-08-16 13:36:39 -0600

    Sort repos by updated_at

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+function compare(a, b) {
+    let dA = Date.parse(a.updated_at);
+    let dB = Date.parse(b.updated_at);
+    if (dA > dB) return -1;
+    if (dB > dA) return 1;
+    return 0;
+}
+
 function sendAjax() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var repos = JSON.parse(this.responseText);
+            repos.sort(function(a, b) {
+                return Date.parse(b.updated_at) - Date.parse(a.updated_at);
+            }); // Sort by updated_at
             repos.forEach(repo => {
                 // Some of this is temporary
                 var section = document.createElement("section");
