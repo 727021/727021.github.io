cb45cf85294519e9405ba8232bd9ff1ca470304b
727021 <1834544+727021@users.noreply.github.com>
2019-09-13 14:57:05 -0600

    Add links to stargazers and forks

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const DEBUG = false;
+
 function compare(a, b) {
     let dA = Date.parse(a.updated_at);
     let dB = Date.parse(b.updated_at);
@@ -11,17 +13,51 @@ function sendAjax() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var repos = JSON.parse(this.responseText);
+            if (DEBUG) console.log(this.responseText);
             repos.sort(function(a, b) {
                 return Date.parse(b.updated_at) - Date.parse(a.updated_at);
             }); // Sort by updated_at
+            var main = document.getElementsByTagName("main")[0];
+            while (main.firstChild != null)
+                main.removeChild(main.firstChild);
             repos.forEach(repo => {
-                // Some of this is temporary
                 var section = document.createElement("section");
-                var a = document.createElement("a");
-                a.setAttribute("href", repo.html_url);
-                a.appendChild(document.createTextNode(repo.name));
-                section.appendChild(a);
-                document.getElementsByTagName("main")[0].appendChild(section);
+                var repoName = document.createElement("span");
+                repoName.appendChild(document.createTextNode(repo.name))
+                repoName.classList.add("repoName");
+                repoName.onclick = function() {
+                    window.location = repo.html_url;
+                }
+                section.appendChild(repoName);
+                section.onclick = function(e) {
+                    if (e.target != this) return;
+                    window.location = repo.html_url;
+                }
+                var star = document.createElement("i");
+                star.classList.add("fas");
+                star.classList.add("fa-star");
+                star.classList.add("gitStar");
+                star.onclick = function() {
+                    window.location = `${repo.html_url}/stargazers`;
+                }
+                star.title = "Stargazers";
+                var sg = document.createElement("sub");
+                sg.appendChild(document.createTextNode(repo.stargazers_count));
+                star.appendChild(sg);
+                section.appendChild(star);
+                var fork = document.createElement("i");
+                fork.classList.add("fas");
+                fork.classList.add("fa-code-branch");
+                fork.classList.add("gitFork");
+                fork.onclick = function() {
+                    window.location = `${repo.html_url}/network/members`;
+                }
+                fork.title = "Forks";
+                var f = document.createElement("sub");
+                f.appendChild(document.createTextNode(repo.forks_count));
+                fork.appendChild(f);
+                section.appendChild(fork);
+                main.appendChild(section);
             });
         }
     }
