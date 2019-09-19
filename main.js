93b72e6473dda2a2df0d5b13aba2a472aa271417
727021 <1834544+727021@users.noreply.github.com>
2019-09-19 16:11:06 -0600

    Repos load from Github

--- main.js
+++ main.js
@@ -55,6 +55,13 @@ function sendAjax() {
                         break;
                 }
             }
+            populateTab("cs", cs);
+            populateTab("cpp", cpp);
+            populateTab("java", java);
+            populateTab("web", web);
+            populateTab("other", other);
+
+            $('[data-toggle="tooltip"]').tooltip();
         }
     }
     xhttp.open("GET", "https://api.github.com/users/727021/repos", true);
@@ -62,22 +69,44 @@ function sendAjax() {
 }
 sendAjax();
 
-function popCsharp(arr) {
+function populateTab(tabId, arr) {
+    if (DEBUG) {
+        console.log(`In populateTab(${tabId}, arr):`);
+        console.log(arr);
+    }
 
-}
+    if (arr.length == 0) {
+        $(`#${tabId}`).html("No projects.")
+        return;
+    }
 
-function popCplusplus(arr) {
+    var tabList = $(`#${tabId} #list-tab`);
+    var tabContent = $(`#${tabId} #nav-tabContent`);
+    tabList.html("");
+    tabContent.html("");
+    arr.forEach(repo => {
+        id = String(repo.id);
 
-}
+        tabList.append(`<a class="list-group-item list-group-item-action" id="list-${id}-list" data-toggle="list" href="#list-${id}" role="tab" aria-controls="${id}">${repo.name}</a>`);
 
-function popJava(arr) {
-
-}
-
-function popWeb(arr) {
-
-}
-
-function popOther(arr) {
-
+        tabContent.append(
+            `<div class="tab-pane fade" id="list-${id}" role="tabpanel" aria-labelby="list-${id}-list">
+            <div class="row">
+                <div class="col-8">
+                    <h3>${repo.name} <small class="text-muted"><small>Updated ${new Date(repo.updated_at).toLocaleDateString("en-US", {day: "numeric", year: "numeric", month: "short"})}</small></small></h3>
+                    </div>
+                    <div class="col-4">
+                        <div class="btn-toolbar float-right" role="toolbar">
+                            <div class="btn-group mr-2" role="group">
+                                <a data-toggle="tooltip" data-placement="top" title="Stargazers" role="button" class="btn btn-secondary" href="${repo.html_url}/stargazers"><i class="fas fa-star"><sub>${repo.stargazers_count}</sub></i></a>
+                                <a data-toggle="tooltip" data-placement="top" title="Forks" role="button" class="btn btn-secondary" href="${repo.html_url}/network/members"><i class="fas fa-code-branch"><sub>${repo.forks_count}</sub></i></a>
+                                <a data-toggle="tooltip" data-placement="top" title="Source" role="button" class="btn btn-primary" href="${repo.html_url}"><i class="fas fa-code"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                ${(repo.description == null) ? "" : repo.description}
+            </div>`
+        );
+    });
 }
