9d12cba5579dc850d6c2c627a59f9e9831d3835a
727021 <1834544+727021@users.noreply.github.com>
2019-09-24 11:30:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function getGit(desc = {}) {
 
             $('[data-toggle="tooltip"]').tooltip();
         }
-        else if (this.readyState == 4 && this.status == 403) {
+        else if (this.readyState == 4 && this.status != 200) {
             ["cs","cpp","java","web","other"].forEach(id => {
                 $(`#${id}`).html("Failed to load data. Click <a href='https://github.com/727021?tab=repositories'>here</a> for a list of projects.")
             });
