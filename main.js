514e8ba612e01f52b07bb4067792fe5d88630e35
727021 <1834544+727021@users.noreply.github.com>
2019-09-25 10:35:04 -0600

    Replace footer with hidden top link

--- main.js
+++ main.js
@@ -105,3 +105,27 @@ function populateTabs(tabIds, arr, desc = {}) {
         });
     });
 }
+
+$(function() {
+    const scrollPos = 50;
+
+    $("#noscript").hide();
+
+    $("a[href='#top']").click(function(e) {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        return false;
+    });
+
+    $(document).scroll(function(e) {
+        if ($(document).scrollTop() >= scrollPos) {
+            $("a[href='#top']").fadeIn();
+        } else {
+            $("a[href='#top']").fadeOut();
+        }
+    });
+
+    if ($(document).scrollTop() >= scrollPos) {
+        $("a[href='#top']").show();
+    }
+});
