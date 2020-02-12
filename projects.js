9a8464d913a48b30674d85d94b406b3c217c49f4
727021 <1834544+727021@users.noreply.github.com>
2020-02-12 10:50:41 -0700

    Add resume

--- projects.js
+++ projects.js
@@ -18,11 +18,12 @@ $(function() {
             $(`#${$(this).attr("aria-labelledby")} #angle`).removeClass("fa-minus").addClass("fa-plus")
         })
 
+        // Initialize tooltips
         $('[data-toggle="tooltip"]').tooltip()
     }
 
     function getGitInfo(projects, currentIndex = 0) {
-        if (currentIndex >= projects.length) return
+        if (currentIndex >= projects.length) return $('#projectsAccordion').children().first().remove()
         let project = projects[currentIndex]
         project.id = project.title.replace(/ /g, '-')
         if (project.repoName) {
@@ -65,15 +66,11 @@ $(function() {
                             <h5 class="mb-0"><a data-toggle="collapse" data-parent="#projectsAccordion" href="#${project.id}" aria-expanded="false" aria-controls="${project.id}"><i id="angle" class="fas fa-plus"></i> ${project.title}</a></h5>
                             <small class="text-muted">${project.subtitle}</small>
                         </div>
-                        <div class="col-2 text-right">
-                            ${json.homepage ? `<a target="_blank" href="${json.homepage}" class="btn btn-secondary" role="button" data-toggle="tooltip" title="Website"><i class="fas fa-desktop"></i></a>` : ''}
-                            <a target="_blank" href="${json.html_url}" class="btn btn-primary" role="button" data-toggle="tooltip" title="Github"><i class="fab fa-github"></i></a>
-                        </div>
                     </div>
                 </div>
                 <div id="${project.id}" class="collapse in" role="tabpanel" aria-labelledby="${project.id}-Header">
                     <div class="card-body">
-                        ${marked(`${project.description}\n\n---\n\nTechnologies used:\n\n- ${project.technologies.join('\n- ')}`)}
+                        ${marked(`${project.description}${(project.technologies.length == 0) ? "" : `\n\n---\n\nTechnologies used:\n\n- ${project.technologies.join('\n- ')}`}`)}
                     </div>
                 </div>
             </div>
@@ -89,6 +86,5 @@ $(function() {
     .then(response => { return response.json() })
     .then(myJSON => {
         getGitInfo(myJSON)
-        $('#projectsAccordion').children().first().remove()
     })
 })
